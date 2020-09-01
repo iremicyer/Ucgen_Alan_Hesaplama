4e9134a50ffce43f064b96938cccf84739549d3d
İREM İÇYER <62353089+iremicyer@users.noreply.github.com>
2020-09-01 18:58:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ucgen_Alan_Hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ucgenin yuksekligini giriniz:");
+            double yukseklik = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("ucgenin taban uzunlugunu giriniz:");
+            double taban = Convert.ToDouble(Console.ReadLine());
+
+            double sonuc = (taban * yukseklik) / 2;
+            Console.Write("ücgenin alani:" + sonuc);
+        }
+    }
+}
